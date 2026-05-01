81b33d6673b4dcadd923041ff486eabdc492fb4c
celia-maz <celia.mazouzi@se.univ-bejaia.dz>
2026-05-01 14:01:37 +0100

    fix: Dockerfile - install PHP extensions, fix composer scripts

--- appli_v2/backend/dump_clean.sql
+++ appli_v2/backend/dump_clean.sql
@@ -1040,4 +1040,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-01 13:30:29
+-- Dump completed on 2026-05-01 13:34:18
